b4183262f3fb10705dc5995ed919bd3f2dae0804
mayankdaruka <53444658+mayankdaruka@users.noreply.github.com>
2021-06-05 02:21:16 -0500

    initial commit with api call etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { token } from "./token";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [fixtures, setFixtures] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://cricket-live-data.p.rapidapi.com/fixtures-by-date/2021-06-05",
+      headers: {
+        "x-rapidapi-key": token,
+        "x-rapidapi-host": "cricket-live-data.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((res) => {
+        // console.log(res);
+        setIsLoaded(true);
+        console.log(res.data);
+        setFixtures(res.data);
+      })
+      .catch((err) => console.error(err));
+
+    // fetch(`https://cricket.sportmonks.com/api/v2.0/scores?api_token=${token}`)
+    //   .then((res) => res.json())
+    //   .then((result) => console.log(result));
+  }, []);
+  return <div> Hello </div>;
 }
 
 export default App;
